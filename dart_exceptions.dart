b053ee5d0d2ba38efb85bbb6bb3833f38e510054
Theo Pinheiro <19900@bemol.local>
2024-11-04 10:22:27 -0400

    first commit

---
+++ dart_exceptions.dart
@@ -0,0 +1,44 @@
+import 'controllers/bank_controller.dart';
+import 'models/account.dart';
+
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+
+//   // Observando resultado
+//   print(result);
+// }
+
+void main() {
+  print("Started main");
+  functionOne();
+}
+
+functionOne() {
+  print("Function One started!");
+  functionTwo();
+  print("Function One is over");
+}
+
+functionTwo() {
+  print("Function Two started...");
+  for (int count = 0; count < 6; count++) {
+    print(count);
+  }
+  print("Finshed function two");
+}
